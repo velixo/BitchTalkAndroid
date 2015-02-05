cbb84e748c00c20e9bdad29ba695d8d9de915552
Vilhelm <v.lundqvist94@gmail.com>
2015-02-05 17:53:40 +0100

    Removed old commented statements

--- app/src/main/java/com/velixo/bitchtalkandroid/statics/StaticVariables.java
+++ app/src/main/java/com/velixo/bitchtalkandroid/statics/StaticVariables.java
@@ -1,15 +1,6 @@
 package com.velixo.bitchtalkandroid.statics;
 
 public abstract class StaticVariables {
-	/** Variables used when a command is sent from the server to a client*/
-//	public final static String SERVERWOOLOOLOO = "/:woolooloo";
-//	public final static String SERVERBOSSASSBITCH = "/:bossassbitch";
-//	public final static String SERVERWHATSGOINGON = "/:whatsgoingon";
-//	public final static String SERVERMOVEBITCHGETOUTDAWAY = "/:movebitchgetoutdaway";
-//	public final static String SERVEROPEN = "/:open";
-//	public final static String SERVERCELEBRATE = "/:celebrate";
-//	public final static String SERVERDOYOUSPEAKIT = "/:doyouspeakit";
-	
 	/** Variables used when a command is sent from a client to the server*/
 	public final static String HELP = "/help";
 	public final static String MUTE = "/mute";
@@ -21,15 +12,4 @@ public abstract class StaticVariables {
 	public final static String BITCHLIST = "/bitchlist";
     public final static String DISCONNECT = "/disconnect";
 	//TODO add ban
-	
-//	public final static String WOOLOOLOO= "/woolooloo";
-//	public final static String WHATSGOINGON= "/whatsgoingon";
-//	public final static String BOSSASSBITCH = "/bossassbitch";
-//	public final static String OPEN= "/open";
-//	public final static String CELEBRATE = "/celebrate";
-//	public final static String DOYOUSPEAKIT = "/doyouspeakit"; 
-	
-//	public final static String BITCHSAYMYNAME = "/bitchsaymyname";
-//	public final static String GAFFELTRUCK = "/gaffeltruck";
-//	public final static String RETARDBIRD = "/retardbird";
 }
